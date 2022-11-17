65460ef355b7c32484bbdb3622a3bcdb10bb0560
SatoshiVision <wenqinghe1016@gmail.com>
2022-11-17 09:23:22 +0800

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+import csv_reader
+import linear_process
+class read_csv():
+    def __init__(self):
+        self.data = None
+
+    def read(self):
+        if not self.data:
+            pass
+
+    def _read_csv(self,name='data.csv'):
+        pass
+
+class process_data():
+    def __init__(self):
+
